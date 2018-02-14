d4b811ac9a05abfcbf9105a3953d1d8b7a811314
Chitale, Yash Hemant <ychitale3@gatech.edu>
2018-02-13 19:18:58 -0500

    Update read_block_from_sensor.py

--- read_block_from_sensor.py
+++ read_block_from_sensor.py
@@ -1,6 +1,6 @@
 from smbus2 import SMBusWrapper
 
-with SMBusWrapper as bus:
+with SMBusWrapper(1) as bus:
 
     block= bus.read_i2c_block_data(0x40, 00000, 16)
-    print (block)
+    print(block)
